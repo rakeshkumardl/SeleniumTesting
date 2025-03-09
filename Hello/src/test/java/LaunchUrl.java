6e48e8f8e2275bc6ae2cba218437ecd01b5a3d01
rakeshkumardl <rakeshkumar15041989@gmail.com>
2025-03-09 12:45:12 +0530

    git Commit

---
+++ Hello/src/test/java/LaunchUrl.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchUrl {
+
+    public static void main(String[] args) {
+        // Set the path to the chromedriver executable
+        System.setProperty("webdriver.chrome.driver", "D:\\chromeDriver\\chromedriver.exe");
+
+        // Initialize the ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Launch the URL
+        driver.get("http://www.google.com");
+
+        // Close the browser
+        driver.quit();
+    }
+}
